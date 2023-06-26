5f9b625d8af6d2cf7908a6339c92c9c1b0bc776b
Md Mamatajur Rashed <soinik.us.army@gmail.com>
2023-06-26 14:07:56 -0500

    update api

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-// const post, comment/ book rout andf file 
+// const post, comment/ book rout and file 
 
 router.use('/users', userRoutes)
 
