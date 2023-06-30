28ae48ed0ad222f0ac7f252a41314a15fa7d9db4
Arael <araeltiejerina@gmial.com>
2023-06-29 19:32:21 -0500

    heroku deployment

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const express = require('express');
-
+const path = require('path');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
-const routes = require('./routes');
-
 const routes = require('./controllers');
 
 const sequelize = require('./config/connection');
