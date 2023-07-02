923ff62c2b64917077ecaba1ab33d25119734664
Md Mamatajur Rashed <soinik.us.army@gmail.com>
2023-07-01 20:03:42 -0500

    update

--- models/Book.js
+++ models/Book.js
@@ -1,1 +1,32 @@
-// book 
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+class Book extends Model { }
+
+Book.init({
+    authors: [
+        {
+            type: String,
+        },
+    ],
+    description: {
+        type: String,
+        required: true,
+    },
+    // saved book id from GoogleBooks
+    bookId: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String,
+    },
+    link: {
+        type: String,
+    },
+    title: {
+        type: String,
+        required: true,
+    },
+});
+
+module.exports = Book;
