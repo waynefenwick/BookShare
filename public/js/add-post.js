12852120165e391294703f2137f08c12348e3c39
Arael <araeltiejerina@gmial.com>
2023-07-03 18:25:30 -0500

    most functions are working need to add the api part and figure out how to diplay the users post under their dashboard

--- public/js/add-post.js
+++ public/js/add-post.js
@@ -3,8 +3,8 @@ async function newFormHandler(event) {
   event.preventDefault();
 
   // Get the title and post content from the form
-  const title = document.querySelector('input[name="post-title"]').value;
-  const post_content = document.querySelector('input[name="post-content"]').value;
+  const title = document.querySelector('input[name="book-title"]').value;
+  const post_content = document.querySelector('textarea[name="post-content"]').value;
 
   // Send the title and post content to the server
   const response = await fetch(`/api/posts`, {
@@ -19,12 +19,10 @@ async function newFormHandler(event) {
   });
 // If the response is ok, reload the page, otherwise display the error
   if (response.ok) {
-    document.location.replace("/dashboard");
+    document.location.replace("/");
   } else {
     alert(response.statusText);
   }
 }
 // Listen for the submit button to be clicked
-document
-  .querySelector(".new-post-form")
-  .addEventListener("submit", newFormHandler);
+document.querySelector("#newPostForm").addEventListener("submit", newFormHandler);
