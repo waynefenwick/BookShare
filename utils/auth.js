f8355d00569b97512d14ab8ebc733f131e83e7ee
Wayne <waynefenwick@gmail.com>
2023-07-04 13:11:44 -0500

    Updates to .handlebars and css files

--- utils/auth.js
+++ utils/auth.js
@@ -1,7 +1,7 @@
 const withAuth = (req, res, next) => {
   // If the user isn't logged in, redirect them to the login route
 if (!req.session.user_id) {
-  res.redirect("/login");
+  res.redirect("/signin");
 } else {
   next();
 }
